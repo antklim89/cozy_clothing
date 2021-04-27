ad142619643033058e3744a1d1729738fd094671
antklim89 <antklim89@mail.ru>
2021-04-27 02:21:29 +0000

    CartList updated

--- src/components/CartList/CartList.tsx
+++ src/components/CartList/CartList.tsx
@@ -1,7 +1,10 @@
 import { FC } from 'react';
 
 
+import { Button } from '../Button';
+
 import { CartItem } from './CartItem';
+import styles from './CartList.module.scss';
 import { CartListPropTypes } from './CartList.types';
 
 import { useCart } from '~/components/CartProvider';
@@ -11,11 +14,21 @@ import { Container } from '~/components/Container';
 export const CartList: FC<CartListPropTypes> = () => {
     const { cart } = useCart();
 
+    const totalPrice = cart.reduce((acc, i) => (acc + i.product.price * i.qty), 0);
     return (
-        <Container component="ul">
-            {cart.map((cartItem) => (
-                <CartItem cartItem={cartItem} key={cartItem.id} />
-            ))}
+        <Container>
+            <ul>
+                {cart.map((cartItem) => (
+                    <CartItem cartItem={cartItem} key={cartItem.id} />
+                ))}
+            </ul>
+            <div className={styles.right}>
+                <p className={styles.totalPrice}>
+                    Total price: $
+                    {totalPrice}
+                </p>
+                <Button size="large">Next</Button>
+            </div>
         </Container>
     );
 };
