c96101b3438bd6d18427b23ea130f6509df4f57a
antklim89 <antklim89@mail.ru>
2021-05-02 12:44:26 +0000

    ignore strapi

--- strapi/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '87ad1df22241563374d683ef83bf047e'),
-    },
-  },
-});
