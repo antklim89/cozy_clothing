b9097ac5e69b6439b7ae2e45664afefc507ad1fa
anton <antklim89@mail.ru>
2021-05-27 19:46:17 +0300

    fixed Header

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 
 import { CartButton } from './CartButton';
 import { Catalog } from './Catalog';
-import style from './Header.module.scss';
+import styles from './Header.module.scss';
 import { NavBar } from './NavBar';
 
 import { Container } from '~/components/Container';
@@ -10,13 +10,16 @@ import { Container } from '~/components/Container';
 
 export const Header: FC = () => {
     return (
-        <div className={style.background}>
-            <Container className={style.container}>
-                <NavBar />
-                <div className="grow" />
-                <Catalog />
-                <CartButton />
-            </Container>
-        </div>
+        <>
+            <header className={styles.background}>
+                <Container className={styles.container}>
+                    <NavBar />
+                    <div className="grow" />
+                    <Catalog />
+                    <CartButton />
+                </Container>
+            </header>
+            <div className={styles.height} />
+        </>
     );
 };
