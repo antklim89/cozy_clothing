232a06fec0af392b4b99fdfe26d645a307d5dc5d
anton <antklim89@mail.ru>
2021-05-28 16:47:32 +0300

    added 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,17 +1,16 @@
-
-// import Layout from '../components/layout';
-// import SEO from '../components/seo';
-
+import { Link } from 'gatsby';
 import { FC } from 'react';
+
+import { Seo } from '~/components/Seo';
 
 
 const NotFoundPage: FC = () => (
-    <div>ERROR</div>
-    // <Layout>
-    //     <SEO title="404: Not found" />
-    //     <h1>404: Not Found</h1>
-    //     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    // </Layout>
+    <main className="center">
+        <Seo title="404: Not found" />
+        <h1>404: Not Found</h1>
+        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <Link to="/">Go Home</Link>
+    </main>
 );
 
 export default NotFoundPage;
