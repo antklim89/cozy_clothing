4e1a56ef6eb79931622806edf00fb78d271d8723
Anton <antklim89@mail.ru>
2021-09-06 19:44:06 +0300

    fixes

--- src/components/Title/Title.types.ts
+++ src/components/Title/Title.types.ts
@@ -1,11 +1,9 @@
 import { HTMLAttributes } from 'react';
 
-import { ContainerProps } from '~/components/Container/Container.types';
-
 
 export interface TitleProps extends HTMLAttributes<HTMLHRElement> {
     component?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
     position?: 'center' | 'tight' | 'left'
-    bottomSpace?: ContainerProps['bottomSpace']
+    bottomSpace?: 'xl'|'lg'|'md'|'sm'|'xs'
 }
 
