2815a1ba6d376e9af017222f28d06b2e4fa9fb13
Anton <31469798+antklim89@users.noreply.github.com>
2021-09-06 00:10:41 +0000

    OrderForm updated

--- src/components/OrderForm/OrderForm.tsx
+++ src/components/OrderForm/OrderForm.tsx
@@ -1,185 +1,199 @@
-import {
-    ChangeEvent, FC, MouseEvent, useEffect, useMemo, useRef, useState,
-} from 'react';
-import { ValidationError } from 'yup';
+import { FC, useEffect, useMemo, useState } from 'react';
 
 import styles from './OrderForm.module.scss';
 import { OrderFormPropTypes } from './OrderForm.types';
 
-import { Input } from '~/components/Input';
+import { cls } from '~/utils';
 
 
-// let formData = {};
+const memoForm = {
+    firstname: '',
+    lastname: '',
+    organization: '',
+    email: '',
+    phone: '',
+    country: '',
+    address: '',
+    details: '',
+};
 
 export const OrderForm: FC<OrderFormPropTypes> = ({ onValidation }) => {
-    // const formRef = useRef<HTMLFormElement>(null);
 
-    const [firstname, setFirstname] = useState('');
-    const [lastname, setLastname] = useState('');
-    const [organization, setOrganization] = useState('');
-    const [email, setEmail] = useState('');
-    const [phone, setPhone] = useState('');
-    const [country, setCountry] = useState('');
-    const [address, setAddress] = useState('');
-    const [details, setDetails] = useState('');
+    const [firstname, setFirstname] = useState(memoForm.firstname);
+    const [lastname, setLastname] = useState(memoForm.lastname);
+    const [organization, setOrganization] = useState(memoForm.organization);
+    const [email, setEmail] = useState(memoForm.email);
+    const [phone, setPhone] = useState(memoForm.phone);
+    const [country, setCountry] = useState(memoForm.country);
+    const [address, setAddress] = useState(memoForm.address);
+    const [details, setDetails] = useState(memoForm.details);
 
-    const form = useMemo(() => ({
-        firstname: (/^[\w]*$/gi).test(firstname),
-        lastname: (/^[\w]*$/gi).test(lastname),
-        organization: (/^[\w\d\s]*$/gi).test(organization),
-        email: (/^[\w\d\s]*$/gi).test(email),
-        phone: (/^[\w\d\s]*$/gi).test(phone),
-        country: (/^[\w\d\s]*$/gi).test(country),
-        address: (/^[\w\d\s]*$/gi).test(address),
-        details: (/^[\w\d\s]*$/gi).test(details),
+    const validation = useMemo(() => ({
+        firstname: (/^[\w]{3,200}$/gi).test(firstname),
+        lastname: (/^[\w]{3,200}$/gi).test(lastname),
+        organization: (/^[\w\d .,;:'"`\-()]{0,200}$/gi).test(organization),
+        email: (/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/gi).test(email),
+        phone: (/^[\d \-()]{3,200}$/gi).test(phone),
+        country: (/^[\w- ]{3,200}$/gi).test(country),
+        address: (/^[\w\d .,;:'"`\-()]{3,2000}$/gi).test(address),
+        details: (/^[\w\d\s.,;:'"`\-()]{0,20000}$/gi).test(details),
     }), [firstname, lastname, organization, email, phone, country, address, details]);
 
-    const [error, setError] = useState<string | null>(null);
-
-    async function validate(data: Record<string, string>) {
-        // const { orderSchema } = await import('~/validation');
-
-        // try {
-        //     const result = await orderSchema.validate(data, { stripUnknown: true });
-        //     onValidation(true);
-        //     setError(null);
-        //     return { result };
-        // } catch (err) {
-        //     if (err instanceof ValidationError) {
-        //         onValidation(false);
-        //         setError(err.message);
-        //         return { error: err.message, result: err.value };
-        //     }
-        //     throw err;
-        // }
-    }
 
     useEffect(() => {
-        onValidation(false);
-        // restoreForm(formRef.current, formData);
-        // validate(formData);
-    }, []);
+        Object.assign(memoForm, {
+            firstname, lastname, organization, email, phone, country, address, details,
+        });
+        const fieldsIsValid = Object.values(validation).every((isValidField) => isValidField);
+        onValidation(fieldsIsValid);
+    }, [firstname, lastname, organization, email, phone, country, address, details]);
 
-    const handleChangeForm = async (e: ChangeEvent<HTMLFormElement>) => {
-        // formData = formToObject(e.currentTarget);
-        // await validate(formData);
-        // console.debug('e: \n', e);
-    };
 
     return (
-        <>
-            {error && (
-                <div className={styles.errorMessage}>
-                    {error}
-                </div>
-            )}
-            <form
-                className={styles.form}
-                onChange={handleChangeForm}
-            >
-                <div className={styles.inputColumns}>
-                    <div className={styles.inputGroup}>
-                        <Input
+        <form className={styles.form} >
+            <div className={styles.inputColumns}>
+                <div className={styles.inputGroup}>
+                    <label
+                        className={cls(
+                            'input-group',
+                            firstname.length > 3 && !validation.firstname && styles.error,
+                        )}
+                    >
+                        First Name: <br />
+                        <input
                             autoComplete="given-name"
-                            label="First Name"
                             name="firstname"
                             type="text"
                             value={firstname}
                             onChange={(e) => setFirstname(e.target.value)}
                         />
-                        <Input
+                        <span>The first name must be between 3 and 200 latin letters long.</span>
+                    </label>
+                    <label
+                        className={cls(
+                            'input-group',
+                            lastname.length > 3 && !validation.lastname && styles.error,
+                        )}
+                    >
+                        Last Name: <br />
+                        <input
                             autoComplete="family-name"
-                            label="Last Name"
                             name="lastname"
                             type="text"
                             value={lastname}
                             onChange={(e) => setLastname(e.target.value)}
                         />
-                    </div>
-                    <div className={styles.inputGroup}>
-                        <Input
+                        <span>The last name must be between 3 and 200 latin letters long.</span>
+                    </label>
+                </div>
+                <div className={styles.inputGroup}>
+                    <label
+                        className={cls(
+                            'input-group',
+                            organization.length > 3 && !validation.organization && styles.error,
+                        )}
+                    >
+                        Organization: <br />
+                        <input
                             autoComplete="organization"
-                            label="Organization"
                             name="organization"
                             type="text"
                             value={organization}
                             onChange={(e) => setOrganization(e.target.value)}
                         />
-                    </div>
-                    <div className={styles.inputGroup}>
-                        <Input
+                        <span>The organization must be between 3 and 200 latin letters long.</span>
+                    </label>
+                </div>
+                <div className={styles.inputGroup}>
+                    <label
+                        className={cls(
+                            'input-group',
+                            email.length > 3 && !validation.email && styles.error,
+                        )}
+                    >
+                        E-mail: <br />
+                        <input
                             autoComplete="email"
-                            label="E-mail"
                             name="email"
                             type="email"
                             value={email}
                             onChange={(e) => setEmail(e.target.value)}
                         />
-                        <Input
+                        <span>The field must be a valid e-mail address.</span>
+                    </label>
+                    <label
+                        className={cls(
+                            'input-group',
+                            phone.length > 3 && !validation.phone && styles.error,
+                        )}
+                    >
+                        Phone number: <br />
+                        <input
                             autoComplete="tel"
-                            label="Phone number"
                             name="phone"
                             type="tel"
                             value={phone}
                             onChange={(e) => setPhone(e.target.value)}
                         />
-                    </div>
-                    <div className={styles.inputGroup}>
-                        <Input
+                        <span>The field must be a valid phone number.</span>
+                    </label>
+                </div>
+                <div className={styles.inputGroup}>
+                    <label
+                        className={cls(
+                            'input-group',
+                            country.length > 3 && !validation.country && styles.error,
+                        )}
+                    >
+                        Country: <br />
+                        <input
                             autoComplete="country-name"
-                            label="Country"
                             name="country"
                             type="text"
                             value={country}
                             onChange={(e) => setCountry(e.target.value)}
                         />
-                    </div>
-                    <div className={styles.inputGroup}>
-                        <Input
+                        <span>The country must be between 3 and 200 latin letters long.</span>
+                    </label>
+                </div>
+                <div className={styles.inputGroup}>
+                    <label
+                        className={cls(
+                            'input-group',
+                            address.length > 3 && !validation.address && styles.error,
+                        )}
+                    >
+                        Address: <br />
+                        <input
                             autoComplete="address-line1"
-                            label="Address"
                             name="address"
                             type="text"
                             value={address}
                             onChange={(e) => setAddress(e.target.value)}
                         />
-                    </div>
+                        <span>The address must be between 3 and 2000 latin letters long.</span>
+                    </label>
                 </div>
-                <div className={styles.inputColumns}>
-                    <div className={styles.inputGroup}>
-                        <Input
+            </div>
+            <div className={styles.inputColumns}>
+                <div className={styles.inputGroup}>
+                    <label
+                        className={cls(
+                            'input-group',
+                            !validation.details && styles.error,
+                        )}
+                    >
+                        Order details: <br />
+                        <textarea
                             className={styles.textarea}
-                            component="textarea"
-                            label="Order details"
                             name="details"
                             value={details}
                             onChange={(e) => setDetails(e.target.value)}
                         />
-                    </div>
+                        <span>The details must be between 3 and 20000 latin letters long.</span>
+                    </label>
                 </div>
-            </form>
-        </>
+            </div>
+        </form>
     );
 };
-
-
-// function restoreForm(form?: HTMLFormElement | null, data?: Record<string, string>): void {
-//     if (!form || !data) return;
-//     Array.from(form).forEach((item) => {
-//         if (item instanceof HTMLInputElement || item instanceof HTMLTextAreaElement) {
-//             if (!data[item.name]) return;
-//             Object.assign(item, { value: data[item.name] });
-//         }
-//     });
-// }
-
-// function formToObject(form: HTMLFormElement): Record<string, string> {
-//     return Array.from(form).reduce<Record<string, string>>((acc, item) => {
-//         if (item instanceof HTMLInputElement || item instanceof HTMLTextAreaElement) {
-//             // return { ...acc, [item.name]: item.value || undefined };
-//             acc[item.name] = item.value
-//             return acc
-//         }
-//         return acc;
-//     }, {});
-// }
