0d6c257b55aa583560fbfaaf5cb26d3a6e74f04e
Anton <antklim89@mail.ru>
2022-12-13 22:41:08 +0300

    fix getTotalPrice

--- src/utils/getTotalPrice.tsx
+++ src/utils/getTotalPrice.tsx
@@ -3,13 +3,9 @@ import { getPrice } from '~/utils';
 
 
 export function getTotalPrice(cart: CartItem[]): string {
-    const totalPrice = cart.reduce((acc, cartItem) => {
-        const price = getPrice({
-            price: cartItem.product.price,
-            discount: cartItem.product.discount,
-            qty: cartItem.qty,
-        });
-        return (acc + Number(price));
+    const totalPrice = cart.reduce((acc, { qty, product }) => {
+        return (acc + (product.price * qty));
     }, 0);
+
     return getPrice({ price: totalPrice });
 }
