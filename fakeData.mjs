306bab63b2b1e16cdb9f2a7723ced2e1793d6b49
Anton <antklim89@mail.ru>
2022-12-19 03:10:24 +0300

    update content

--- fakeData.mjs
+++ fakeData.mjs
@@ -1,29 +1,28 @@
-import fs from 'fs';
 import path from 'path';
 
 import { faker } from '@faker-js/faker';
+import fs from 'fs-extra';
 import _ from 'lodash';
 
 
-const PRODUCT_PER_CATEGORY = 6;
-const CATEGORIES_PER_TYPE = 4;
+const PRODUCT_PER_CATEGORY = 20;
 const FEATURES = 4;
-
-const YEAR_MS = 31557600000;
 
 
 const types = ['men', 'women', 'boys', 'girls'];
-const catalog = types.reduce((acc, type) => ({
-    ...acc,
-    [type]: ['trousers', 'shirts', 'boots', 'sweatshirt', 'jeans', 'underwear'],
-}), {});
+const catalog = {
+    men: ['trousers', 'shirts', 'boots', 'sweatshirt', 'jeans', 'underwear'],
+    women: ['trousers', 'shirts', 'boots', 'sweatshirt', 'jeans', 'underwear', 'blouses', 'dresses'],
+    boys: ['trousers', 'shirts', 'boots', 'sweatshirt', 'jeans', 'underwear'],
+    girls: ['trousers', 'shirts', 'boots', 'sweatshirt', 'jeans', 'underwear', 'dresses'],
+};
 const features = _.times(FEATURES, () => faker.company.catchPhrase());
 
 /**
  * Catalog
  */
-const catalogPath = path.resolve('content/catalog');
-if (!fs.existsSync(catalogPath)) {
+{
+    const catalogPath = path.resolve('content/catalog');
     fs.mkdirSync(catalogPath, { recursive: true });
 
     const fileName = 'catalog.json';
@@ -67,14 +66,14 @@ if (!fs.existsSync(productPath)) {
             fs.mkdirSync(productTypePath, { recursive: true });
         }
 
+        fs.copySync(path.resolve('content/images'), path.resolve(productTypePath, 'images'));
+
         const categories = catalog[type];
 
-        _.sampleSize(categories, CATEGORIES_PER_TYPE).forEach((category) => {
+        categories.forEach((category) => {
             _.times(PRODUCT_PER_CATEGORY, () => {
                 const title = faker.commerce.productName();
-
                 const fileName = `${_.kebabCase(`${category}-${title}`)}.json`;
-
 
                 const product = JSON.stringify({
                     layout: 'product',
@@ -82,7 +81,7 @@ if (!fs.existsSync(productPath)) {
                     hidden: false,
                     discount: _.random(0, 1, true) > 0.5 ? _.random(5, 40, false) : 0,
                     title,
-                    careatedAt: faker.date.between(new Date(), new Date(Date.now() - YEAR_MS)).toISOString(),
+                    createdAt: faker.date.between(new Date(new Date().setFullYear(2003)), new Date()).toISOString(),
                     imagePreview: `images/fake-image-${_.random(1, 6, false)}.jpg`,
                     images: [
                         'images/fake-image-1.jpg',
@@ -92,7 +91,7 @@ if (!fs.existsSync(productPath)) {
                         'images/fake-image-5.jpg',
                         'images/fake-image-6.jpg',
                     ],
-                    price: Number(faker.commerce.price(10, 1000, 2)),
+                    price: parseFloat(faker.commerce.price(10, 1000)),
                     category,
                     body: `${faker.commerce.productDescription()}
                 ${faker.commerce.productDescription()}
