c47521f6ee02225a6bfaed1f5f85d9a306601aee
Anton <antklim89@mail.ru>
2022-12-19 18:09:47 +0300

    remove throwErr

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,4 @@
 export * from './cls';
-export * from './throwErr';
 export * from './getPrice';
 export * from './getTotalPrice';
 export * from './cartStorage';
--- src/utils/throwErr.tsx
+++
@@ -1,3 +0,0 @@
-export const throwErr = (): never => {
-    throw new Error('');
-};
