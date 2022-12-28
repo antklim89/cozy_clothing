946b2f84c216858ac60fbe7e743da8ab58178b13
Anton <antklim89@mail.ru>
2022-12-28 23:44:49 +0300

    add clear console

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -14,7 +14,12 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+import './commands';
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+before(() => {
+    // eslint-disable-next-line no-console
+    console.clear();
+});
